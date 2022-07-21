aff58e1eb62ea3367148299a350d6f3b23019b3c
Vili Vilenius RasPi4 <vili.vilenius@outlook.com>
2022-07-21 18:59:38 +0300

    Import fix

--- src/CameraApp/__main__.py
+++ src/CameraApp/__main__.py
@@ -1,8 +1,11 @@
-from . import API as CameraApp
+try:
+    from . import API as CameraApp
+except ImportError:
+    import API as CameraApp
 
 import sys
 
-args = sys.args[1:]
+args = sys.argv[1:]
 
 if not args:
     CameraApp.hello()
